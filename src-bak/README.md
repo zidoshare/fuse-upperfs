5d023840a19cc28a6420ef183726a75538952548
zido <wuhongxu1208@gmail.com>
2022-02-17 17:52:51 +0800

    remove unused codes

--- src-bak/README.md
+++
@@ -1,5 +0,0 @@
-# 手动编译
-
-```shell
-gcc -Wall -std=c11 `pkg-config fuse3 --cflags --libs`  -o fuseupperfs error.c fuse_ops.c fuseupperfs.c quota.c space.c
-```
--- src-bak/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef UPPER_FUSE_LOG_H
-#define UPPER_FUSE_LOG_H
-
-// 开启 log
-void up_log_enable();
-// 打印log， 在未开启的情况下，不输出
-int up_logf(char* format, ...);
-
-#endif
